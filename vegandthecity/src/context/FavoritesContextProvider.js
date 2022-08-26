247be6d561758b867ae36f14023ac1fe01231737
cristopherturazza <cristopherturazza@gmail.com>
2022-08-26 21:14:51 +0200

    now localstorage works

--- vegandthecity/src/context/FavoritesContextProvider.js
+++ vegandthecity/src/context/FavoritesContextProvider.js
@@ -8,9 +8,21 @@ const FavoritesContextProvider = (props) => {
   const addFavRecipe = (id, title, image) => {
     setFavRecipes([...favRecipes, { id, title, image }]);
   };
+
   const removeFavRecipe = (id) => {
     setFavRecipes(favRecipes.filter((recipe) => recipe.id !== id));
   };
+
+  useEffect(() => {
+    const localData = localStorage.getItem("Favorite Recipes");
+    if (localData) {
+      setFavRecipes(JSON.parse(localData));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("Favorite Recipes", JSON.stringify(favRecipes));
+  }, [favRecipes]);
 
   return (
     <FavoritesContext.Provider
